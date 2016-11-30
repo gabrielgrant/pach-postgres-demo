af83eaaf816a735749043f036629e363257f2fbf
Gabriel Grant <g@briel.ca>
2016-11-29 19:51:05 -0800

    wrap generate_orders.py as a cmd line script

--- generate_orders.py
+++ generate_orders.py
@@ -4,17 +4,36 @@ import csv
 from datetime import datetime, timedelta
 from random import randint, random
 
-# create a batch of orders randomly spaced throughout the batch time period (1h)
-num_orders = 10
-num_customers = 12
-total_batch_time = 60*60  # seconds
-now = datetime.now()
-created_offsets = sorted(random() * total_batch_time for i in range(num_orders))
-for id_, created_offset in enumerate(created_offsets, 1):
-    customer_id = randint(1, num_customers)
-    units = randint(1, 10)
-    created_at = now + timedelta(seconds=created_offset)
-    created_at = str(created_at) + '+00'  # add timezone
-    args = [str(a) for a in [id_, customer_id, units, created_at]]
-    print(','.join(args))
- 
+def generate(days):
+  # create a batch of orders randomly spaced throughout the batch time period (1 day)
+  num_orders = 100 * days
+  num_customers = 12
+  total_batch_time = 24*60*60 * days  # seconds
+  now = datetime.now()
+  created_offsets = sorted((random() * total_batch_time for i in range(num_orders)), reverse=True)
+  print("id,customer_id,units,created_at")
+  for id_, created_offset in enumerate(created_offsets, 1):
+      customer_id = randint(1, num_customers)
+      units = randint(1, 10)
+      created_at = now - timedelta(seconds=created_offset)
+      created_at = str(created_at) + '+00'  # add timezone
+      args = [str(a) for a in [id_, customer_id, units, created_at]]
+      print(','.join(args))
+
+USAGE = 'Usage: generate_orders.py [NUM_DAYS]'
+
+def main(args):
+  if not 0 <= len(args) <= 1:
+    print(USAGE)
+    return -1
+  try:
+    days = int(args[0])
+  except:
+    print(USAGE)
+    return -1
+  generate(days)
+  return 0
+
+if __name__ == "__main__":
+  import sys
+  sys.exit(main(sys.argv[1:]))
