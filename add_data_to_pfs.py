fde57b2798ceee50431e66931e9107def9428091
Gabriel Grant <g@briel.ca>
2016-11-30 08:02:36 -0800

    auto-delete repos before recreating

--- add_data_to_pfs.py
+++ add_data_to_pfs.py
@@ -30,6 +30,10 @@ def commit_customers():
         'pachctl put-file -c customers master customers.csv'.split(),
         stdin=open('data/customers.csv'))
 
+def delete_repos():
+    subprocess.check_output('pachctl delete-repo customers'.split())
+    subprocess.check_output('pachctl delete-repo orders'.split())
+
 USAGE = 'commit_data.py [delete]'
 def main(args):
     if len(args) > 1:
@@ -37,17 +41,20 @@ def main(args):
         return -1
     if len(args) == 1:
         if args[0] == 'delete':
-            prompt = 'Are you sure you want to delete repos "customers" and "orders?"'
+            prompt = 'Are you sure you want to delete repos "customers" and "orders? [y/N]"'
             if raw_input(prompt).lower() == 'y':
-                subprocess.check_output('pachctl delete-repo customers'.split())
-                subprocess.check_output('pachctl delete-repo orders'.split())
+                delete_repos()
             else:
                 return 0
         else:
             print USAGE
             return -1
-    commit_customers()
-    commit_orders()
+    else:
+        prompt = 'Do you want to delete repos "customers" and "orders? [y/N]"'
+        if raw_input(prompt).lower() == 'y':
+            delete_repos()
+        commit_customers()
+        commit_orders()
     return 0
 
 if __name__ == '__main__':
