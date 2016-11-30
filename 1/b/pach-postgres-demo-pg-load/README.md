4cd760212b72450e0bbc4aa8e0c12940192745cf
Gabriel Grant <g@briel.ca>
2016-11-30 07:06:42 -0800

    initial import of  docker image def

---
+++ 1/b/pach-postgres-demo-pg-load/README.md
@@ -0,0 +1,4 @@
+# gabrielgrant/pach-postgres-demo-pg-load
+
+
+Creates DB and loads data
---
+++ 1/b/pach-postgres-demo-pg-load/schema.sql
@@ -0,0 +1,16 @@
+CREATE TYPE pachyderms AS ENUM ('elephant', 'rhino', 'hippo');
+
+CREATE TABLE customers(
+  id SERIAL PRIMARY KEY,
+  name varchar(127),
+  species pachyderms
+);
+
+CREATE TABLE orders(
+  id SERIAL PRIMARY KEY,
+  customer_id integer REFERENCES customers (id),
+  units INT,
+  created_at TIMESTAMP WITH TIMEZONE DEFAULT (now() at time zone 'utc'),
+  shipped_at TIMESTAMP WITH TIMEZONE DEFAULT NULL
+);
+
